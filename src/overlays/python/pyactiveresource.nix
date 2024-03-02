c0a21d2096266e963314e10a1a820422b456d5f9
David Arnold <dgx.arnold@gmail.com>
2024-03-02 13:35:11 +0100

    fix: workaround github api wiredness

--- src/overlays/python/pyactiveresource.nix
+++ src/overlays/python/pyactiveresource.nix
@@ -14,7 +14,9 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "Shopify";
     repo = "pyactiveresource";
-    rev = "v${version}";
+    # github errors on tag fetch:
+    # the given path has multiple possibilities: #<Git::Ref:0x00007fede9e7f5c0>, #<Git::Ref:0x00007fede9e7eda0>
+    rev = "e609d844ebace603f74bc5f0a67e9eafe7fb25e1";
     hash = "sha256-DvM8P+2LXKtDQ6wIPr25t5xPswLP1m7acglYibkz/A4=";
   };
 
