820129d5a3d3083bdfefcc9ea571352a9c6c7a99
David Arnold <dgx.arnold@gmail.com>
2024-03-06 23:17:03 +0100

    fix: add ca properly

--- src/nixos.nix
+++ src/nixos.nix
@@ -48,64 +48,88 @@ in {
         redis_cache = "unix:///run/redis-${cfg.project}-cache/redis.sock";
       });
 
-    # minica --domains '*.localhost,*.frx.localhost'
-    ca = builtins.toFile "ca.crt" ''
+    # minica --domains '*.frx.localhost'
+    ca = builtins.toFile "ca.pem" ''
       -----BEGIN CERTIFICATE-----
-      MIIDQzCCAiugAwIBAgIIZihSICHS/4kwDQYJKoZIhvcNAQELBQAwIDEeMBwGA1UE
-      AxMVbWluaWNhIHJvb3QgY2EgNzk5YmQ5MB4XDTI0MDMwNjE5MDQwNVoXDTI2MDQw
-      NTE4MDQwNVowFjEUMBIGA1UEAwwLKi5sb2NhbGhvc3QwggEiMA0GCSqGSIb3DQEB
-      AQUAA4IBDwAwggEKAoIBAQDUXpNy9YEfNcoJDNsZ4IaV5N7ZOv5/96fKyCs5WD6m
-      FRBqZuT0+FIoCdLEggAVPjgwPqdyeWXNIyxVXjHhAl/5wn4kR5ijfnQwTnIZgxqX
-      8hJZCshgL8xdCWbIbJKUzFdCZllgCfpJtynZIbTz95oKycR71fT0rVDdyPUKYzVe
-      Qrt4nX0TR2NjEQrVQY9SvwD7KGTMaboiLVaxuYI0lgZJJtFvv8Rm8h+qyq2+RmVE
-      bOODsVrswbb5V845F6FOoZlOnGbogbmLEjWjunZijQpfkxmgClkOgIC57aXPVyaM
-      R4ojMGMJp3MuF7arS4aT6lTA4NVM4B3WjFMBDgq8g7axAgMBAAGjgYowgYcwDgYD
-      VR0PAQH/BAQDAgWgMB0GA1UdJQQWMBQGCCsGAQUFBwMBBggrBgEFBQcDAjAMBgNV
-      HRMBAf8EAjAAMB8GA1UdIwQYMBaAFGPwy4elA+T+7Ud6os74hgPhYIEiMCcGA1Ud
-      EQQgMB6CCyoubG9jYWxob3N0gg8qLmZyeC5sb2NhbGhvc3QwDQYJKoZIhvcNAQEL
-      BQADggEBALfE075WUj6tk+2ds3wiODD3AnQz5BV5Z7b+MErpkH1IgT0vNM0lAXeF
-      ohzCgbW50TzfixJeol4oWT/sEO9yATih6kH65arEQ4ga8/jfMZPA9fyE+NL/trSq
-      M5NNbKT8GVf346tWhyHT9hVBjPiM2GDMOdv0bHGy808Qz2jjTRU6vLa7FntQGY5x
-      Sm5n7qZ4Os/jJoVazbfl4h5Uee1KRQweqHQux0xyxsaVcEJUgIVWTcCzXOV2mQKB
-      kF6fNtS00+B+a0deAZV2XDmTGf6PFEvaxflVU5KuC4wjeIMkmhDtbFV2Xeqz8jd1
-      0GQs8/QOW/64g8PkeGtCXtPfwJWWse8=
+      MIIDSzCCAjOgAwIBAgIIGk75NgQUdjEwDQYJKoZIhvcNAQELBQAwIDEeMBwGA1UE
+      AxMVbWluaWNhIHJvb3QgY2EgMWE0ZWY5MCAXDTI0MDMwNjIyMDkzM1oYDzIxMjQw
+      MzA2MjIwOTMzWjAgMR4wHAYDVQQDExVtaW5pY2Egcm9vdCBjYSAxYTRlZjkwggEi
+      MA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDhCp78fn3/Fw1lZPgOXosNnyRH
+      yjonFV6vcnKoVWqAnrSH3WF6dWJXIu3ncKkYmRvo5yofkT/yqeKFU9mecW0k/2yh
+      9fX9qDFRpSdPVnyEUDW7U6/y9tTxDH7gR9Xcjk0LHPsj7+Hq+xka5EFEu0eAbea4
+      miLjN4edtVfZmtTGTNifNFW5yqUQbnHSgnrfq1IxaGR+hThiqxwGp8m0YjnvdCMz
+      WOsEJuTSf1JtXYKpMmP7bKL2GbxlyL7CudRiZ4zkgATxknE9tvCWgMKxzS/n8696
+      Q8DG3v2CScPzeod5EoGTn5Rs1Zl+33f0vcIEFzyydPM/Hk9N+bIf6NXzFEurAgMB
+      AAGjgYYwgYMwDgYDVR0PAQH/BAQDAgKEMB0GA1UdJQQWMBQGCCsGAQUFBwMBBggr
+      BgEFBQcDAjASBgNVHRMBAf8ECDAGAQH/AgEAMB0GA1UdDgQWBBRLBldgiEpXqUvr
+      XE6hm1xE8whPLTAfBgNVHSMEGDAWgBRLBldgiEpXqUvrXE6hm1xE8whPLTANBgkq
+      hkiG9w0BAQsFAAOCAQEAtIzifNoi245X0BW3JGCdOQXJ4U5hufBNvF7tUAEghyFo
+      ez9eihLjg8HlDRjPwQeSDrRsXEC1DfyxpHB9wYkFRbhyKDmqUAVL7ExVEZz2Vy0f
+      /inRYMSGPSpI61xKMotBPRLboAhD5pH9HDrpXwxBD/iBoAHxO4pNHNyJQbvpIJ+l
+      pa69xde5LHQ7/UF+HYPVyBfPFWJP/ElW55poQOJsKjPOKN7+Xr4zShqB6ai5mfhu
+      E8gUmKbc8cD9Sznb7thm+tqRMm/+tIKuf0xASLVLfzg3U0HQejcgm/UEISaGqH5p
+      btDVpTCDuEPT9QA38mQwCgcjpEGGIbi3dcGfGhzD+w==
+      -----END CERTIFICATE-----
+    '';
+    cert = builtins.toFile "cert.pem" ''
+      -----BEGIN CERTIFICATE-----
+      MIIDODCCAiCgAwIBAgIIL6KPg2gKl20wDQYJKoZIhvcNAQELBQAwIDEeMBwGA1UE
+      AxMVbWluaWNhIHJvb3QgY2EgMWE0ZWY5MB4XDTI0MDMwNjIzMDQ0OVoXDTI2MDQw
+      NTIyMDQ0OVowGjEYMBYGA1UEAwwPKi5mcngubG9jYWxob3N0MIIBIjANBgkqhkiG
+      9w0BAQEFAAOCAQ8AMIIBCgKCAQEAv/F6anuNNfkCLfTfF8DW8h98PJlaDevssved
+      rxf8QNoPqLTqKJdcFO9bMpn6QwlsRuIJKfhEDnlKIvNKOEZooUbljhVyqbRlYYq1
+      KDZtc4QT9rJC9T3Cp5Pm96nHm1fOk0ePthTd6Bt32nxRDpx9+4vYHuIlTiuIJfLS
+      fy/otbPQRB0s4e9bXq7x08cELW5czwRE5whibBYD7geHdfHy9wU7l+OVkfKPmBHp
+      oTmdc5ctPEX0U87sRC0ZSmf6tOU13EXwBY+srvJxBIAQwv8SPsj1IBdPOFOuf36F
+      DiniwU/QruKbs3Z9mqVQQULDgEl1B+9pOlni1VX32Mzzeyf4dQIDAQABo3wwejAO
+      BgNVHQ8BAf8EBAMCBaAwHQYDVR0lBBYwFAYIKwYBBQUHAwEGCCsGAQUFBwMCMAwG
+      A1UdEwEB/wQCMAAwHwYDVR0jBBgwFoAUSwZXYIhKV6lL61xOoZtcRPMITy0wGgYD
+      VR0RBBMwEYIPKi5mcngubG9jYWxob3N0MA0GCSqGSIb3DQEBCwUAA4IBAQArk3W8
+      pdnr2idt/b6Gqqo+YtHI8yApIIXnxtElK9hQFkLd0bdDtmhZ6gBGNk2jg9DSUyLU
+      jZuNGSGTS4TWz8iu5l/ypc2SmVhQ6XG4+1ZtlAVLBtBhqy4CcRS5B4taClk4q50o
+      megWX4+Wf1oCC5k7286UrB9CJHWOapCG+PHJKREj9s/9QDd3dFGsmqj25cdCTjxY
+      CFAkN9ecby8/Yj9FEHe8g2yfW6Zhedtyhwt5LvqFWbN9/6A5TcqDKvIEX3ZHALJF
+      D84K6z3JWskuX/8K57Y03FF66QOkJ6zgFWZz3/ptf+/X+qwzLq3uJerLnSlq/K7q
+      TdydBj+hpAPL0U5T
       -----END CERTIFICATE-----
     '';
     key = builtins.toFile "key.pem" ''
       -----BEGIN RSA PRIVATE KEY-----
-      MIIEpQIBAAKCAQEA1F6TcvWBHzXKCQzbGeCGleTe2Tr+f/enysgrOVg+phUQambk
-      9PhSKAnSxIIAFT44MD6ncnllzSMsVV4x4QJf+cJ+JEeYo350ME5yGYMal/ISWQrI
-      YC/MXQlmyGySlMxXQmZZYAn6Sbcp2SG08/eaCsnEe9X09K1Q3cj1CmM1XkK7eJ19
-      E0djYxEK1UGPUr8A+yhkzGm6Ii1WsbmCNJYGSSbRb7/EZvIfqsqtvkZlRGzjg7Fa
-      7MG2+VfOORehTqGZTpxm6IG5ixI1o7p2Yo0KX5MZoApZDoCAue2lz1cmjEeKIzBj
-      CadzLhe2q0uGk+pUwODVTOAd1oxTAQ4KvIO2sQIDAQABAoIBAQCQZqZ32nsrz5VK
-      xhUM6WBZ97+XkcePF8Rd2/GYEmq230fEMaao81hZpSRNPd/0kdP+6ftNmUIhVDNG
-      8L+Vsdm0qAzBenVNZiR23EA1HLIucwkKxows6xNYh5X93eVli/QhUBqhdOdczFCG
-      Nacm5Es33q3dTkQ7QsXjqEsF/yNArX5KnRrXk0c7W+MiuMdUOGrgYhMWPZWIzv+O
-      ejjUnTegr64dPdQFwWRCi2+4HEp3dUAkjapN3xy99E9kb5ZP0bSu3/XMRHw/kRFM
-      an3B1sQKks9ow1QY4baQVWOjfO/r3wIOq2gyJUrZ/XOSj8ZlEs8pLBBtjQVPCs7z
-      otiBuS6JAoGBAO2XwbB8QTJNWvgUbTxRgd16jTclwYmQPxd+rr6n6zU4nhPNWHuK
-      YGnvWeVgXcs/fqT4NdipUQewKMSEXOXeWfdS6OVpSQi88udMmkuSJpwRu4kqAxaN
-      YTn+n8bIRgptwiSy3m6w7pbOZJbitO5+meV+xyz+DATUU8Dnuv07HIDXAoGBAOTS
-      jrfwb4YasPgSJFCKUqQT92iRYshD3f+bftAaUodzgnZ8RQ+xEqhewXAvygwlobbd
-      fCLGpLTFk2Fj8LqjQlVJZtfi0sB6CKS+w11a3rF81GStNW58Q15L7Ja9zkc2TJp/
-      5zMVGOKg2BPG3lpJif8HHfT1EyjCtKuF2z/xDKu3AoGBAI7vhFvrdMGRXg/vIgRu
-      uKUIrFooAbYDrKEy2mfi15LuG9On7vprtjMlBr0C64pyCXuvw50zx4bLiMspIkY8
-      LX2oSGpzm7eBDNTv9cdPBqFP3bXYK4FuzrY4I+FP7Ssq+uhfH4gSM4dQZZ2Mh/MP
-      AQDa83Jy58IkBrr/7jOYUz6DAoGBAL+Hnv5HupG9dbOvO8ZOga3lKInWZ6DJbeCt
-      /w+heML35JPYoNXpmXxlE/UbiztvpFS0P1u8edD2L86tIKqYTMWmkvbRXSbO/r0n
-      D8/sZ8qYeg9rC/ZW/OzJEFd8uFTTFnub46HWXuYTS8oCA8v/Egrtoh2PpWHQ1/la
-      KZLz6W8xAoGAWLNa4h0fN3iPJf49I/b+jWjIX0MJaHrycFSlmZU8ESxWITLJdA0/
-      VMVFKjGEzH/ji639kBoQ2WeK/Crc70CrEp6v0iAt9Dtsg04CxCCToB0bodirT5jp
-      UE6Ygerk1W6DESXAPy5CRJjca5KxunRRETqhMXdPcTcrJRacngQYENQ=
+      MIIEowIBAAKCAQEAv/F6anuNNfkCLfTfF8DW8h98PJlaDevssvedrxf8QNoPqLTq
+      KJdcFO9bMpn6QwlsRuIJKfhEDnlKIvNKOEZooUbljhVyqbRlYYq1KDZtc4QT9rJC
+      9T3Cp5Pm96nHm1fOk0ePthTd6Bt32nxRDpx9+4vYHuIlTiuIJfLSfy/otbPQRB0s
+      4e9bXq7x08cELW5czwRE5whibBYD7geHdfHy9wU7l+OVkfKPmBHpoTmdc5ctPEX0
+      U87sRC0ZSmf6tOU13EXwBY+srvJxBIAQwv8SPsj1IBdPOFOuf36FDiniwU/QruKb
+      s3Z9mqVQQULDgEl1B+9pOlni1VX32Mzzeyf4dQIDAQABAoIBAFJ9L9P1VyGb6zDe
+      7MWjjXyuxVCxwv76plXfjrfCAhnI3TPu1DcvnVYt666ad/A5a3wnBjMwS+dAfvj4
+      P6xhrfOpqVvfodCHA47g8qvegDlFyOKbh6UnrrEgIgHLyEndeZzYA04IN7nZnC+c
+      f6Vc4dOqLO5Q2CF5GeDdM1OPvl3fsiDgE9f0WZNuViBTgW05fU8Ntk6U5wLjHHHb
+      2h6zqPVswaqilp0Uik9jHLPv+WzZbAbpg8TduBV7OxL7yZzN4GEPMFuIDjqcpJcM
+      EXjxJoPqsuGM0gCL5Cu6pnIwMuOUjYRqdNwXesY3/Eurb2P/wHIEUN5i89VG+gWv
+      Kc7AL8ECgYEAw88qPEXwHCNKz/IWlfO2rD29HymuCCOzzRu25+Bt4E3cy9vtbTW7
+      CLDPaEDehntflgfmSVTj8y4ZhJG28gTMbqOwFT6uDnGjLye0MAjTvjwskmBwZ898
+      fSH/RjSKf2Uhryq29zyP9BHY8aA7maaqaOSlRG4yn0DJ3UxsWAYx6oUCgYEA+vIW
+      qdJXFxh9MhBIxPeRADZI6E/Al78ZCtRidO6aIr0s2Axq7PJOmeQ9AWq2Ikc9qdWo
+      h9gQ1uZKjEaKU4hokktucIsyfzGH8gQOYSZYKVgs8Zf/gq26taUq8yWVx7CR/n7a
+      fZ70N8USVZjujuaix3FsYlgDEYOrkXB9XtKuUTECgYEAvGZKu/2K3nfylovWwWbi
+      P22T1zUSNtCrQlFFNmvRLH2eUSOFmYuWLvF4TlYEBZ8VRFTLdYlRXnzfnpdZUBnf
+      3SBv3rEVZd5e5ZMtIv6LRUG+nQRfgvK3U+rvJEyPaa4Tr+fIba/+zhaSB0JlthwP
+      YKeGgIYK/QGoeN38bOFhC8UCgYBATDcyXAqkiEnLwhBjJ2unukEEBSs7tmMcOz7e
+      5yPnMsGCuevLumoZVDmtW0I/ljFeirgb1mi6J1eCibB3psPkeB9cBs5xeKd0g0WL
+      7t83+LfLTz7QpOLqF9/hXQf7mmpN4wLonQnGIGCKPh0h0EZ1A8LZj+N5YVtccI4u
+      9ZxkgQKBgF1Z+SMIZYYFYAS7pDf8BVWFZrUoIgNkX+RjAFtSlvkQJTYeQNx/ePun
+      2Qtz4OBWSymnyyDhT4DdJ56FdxFk3Zqh5smNPBcEIjGIov1yW5rBBopg/LUBsKH8
+      mTFwxPCr0voiiPqmKMmupOPeyr8DT+N65Vmy58mUgywlD6QIizBH
       -----END RSA PRIVATE KEY-----
     '';
   in {
     _file = ./tests.nix;
     options.services.nginx.virtualHosts = lib.mkOption {
       type = lib.types.attrsOf (lib.types.submodule {
-        sslTrustedCertificate = lib.mkDefault "${sslPath}/ca.crt";
-        sslCertificate = lib.mkDefault "${sslPath}/ca.crt";
+        sslTrustedCertificate =
+          lib.mkDefault
+          config.environment.etc."ssl/certs/ca-certificates.crt".source;
+        sslCertificate = lib.mkDefault "${sslPath}/cert.pem";
         sslCertificateKey = lib.mkDefault "${sslPath}/key.pem";
         enableACME = mkTestOverride false;
       });
@@ -129,7 +153,7 @@ in {
       users.mutableUsers = false;
       networking = {
         firewall.enable = false;
-        # chrome and other browsers don't show a warining screen for self signed certs on localhost
+        # chrome and other browsers don't show a warning screen for self signed certs on localhost
         domain = mkTestOverride "localhost";
         hostName = mkTestOverride "frx";
         # workarround
@@ -165,12 +189,12 @@ in {
         enable = mkTestOverride true;
         wheelNeedsPassword = false;
       };
-      systemd.services."create-wildcard-localhost-cert" = {
-        description = "Create a wildcard certificate for *.localhost";
+      systemd.services."create-wildcard-frx.localhost-cert" = {
+        description = "Create a wildcard certificate for *.frx.localhost";
         script = ''
-          cp ${ca} ca.crt
+          cp ${cert} cert.pem
           cp ${key} key.pem
-          chmod 644 ca.crt
+          chmod 644 cert.pem
           chmod 640 key.pem
         '';
 
@@ -179,7 +203,7 @@ in {
         after = ["systemd-tmpfiles-setup.service"];
         unitConfig = {
           Before = ["multi-user.target" "nginx.service"];
-          ConditionPathExists = "!${sslPath}/ca.crt";
+          ConditionPathExists = "!${sslPath}/cert.pem";
         };
 
         serviceConfig = {
