cd176a74f913497a3780ab48a8bf92fb89560062
David <dgx.arnold@gmail.com>
2024-07-02 19:35:53 +0200

    update(crm): to v1.17.0

--- apps/sources/crm.nix
+++ apps/sources/crm.nix
@@ -1,16 +1,16 @@
 {
   pname = "crm";
-  version = "v1.14.0";
+  version = "v1.17.0";
   meta = {
-    url = "https://github.com/frappe/crm/releases/tag/v1.14.0";
-    description = "Sources for crm (v1.14.0)";
+    url = "https://github.com/frappe/crm/releases/tag/v1.17.0";
+    description = "Sources for crm (v1.17.0)";
   };
   src = builtins.fetchTree {
     type = "git";
     url = "git@github.com:frappe/crm.git";
     submodules = true;
-    narHash = "sha256-0E754geSD/LUcjUGr3WwcD9CAHaTUn+MFnonwXy5noM=";
-    rev = "a1ec478744e38ed7666e784a23613df460a12c56";
+    narHash = "sha256-HHviUSgtV8r/AXtqEku913FkM8zQ9bb4TKHVZwdg3L0=";
+    rev = "064106e76cab8917ba7f3b41fba88fa53444ba68";
   };
   passthru = builtins.fromJSON ''{}'';
 }
