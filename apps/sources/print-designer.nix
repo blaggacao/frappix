02a97c48be3ca50197989081dcf38dd8488482a6
David <dgx.arnold@gmail.com>
2024-07-02 19:51:32 +0200

    update(print-designer): to v1.3.3

--- apps/sources/print-designer.nix
+++ apps/sources/print-designer.nix
@@ -1,16 +1,16 @@
 {
   pname = "print-designer";
-  version = "20240317.225753";
+  version = "v1.3.3";
   meta = {
-    url = "https://github.com/frappe/print_designer/commit/f87b1aa2de3b5c720f24cc91fddc614a89d219a0";
-    description = "Sources for print-designer (20240317.225753)";
+    url = "https://github.com/frappe/print_designer/releases/tag/v1.3.3";
+    description = "Sources for print-designer (v1.3.3)";
   };
   src = builtins.fetchTree {
     type = "github";
     owner = "frappe";
     repo = "print_designer";
-    narHash = "sha256-/oAMeSt/VVXDiF0ov52Qm9IcpsX22XA//kywxDTJ0Ww=";
-    rev = "f87b1aa2de3b5c720f24cc91fddc614a89d219a0";
+    narHash = "sha256-2NyE+ENtzVKvOQDhPrYbS03sJJnZ9HF2MvFXbdIZAW8=";
+    rev = "59302de3c0a11558823c21e26b704e0a7e16cc3b";
   };
   passthru = builtins.fromJSON ''{}'';
 }
