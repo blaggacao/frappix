cb40ba4e2b5f907fc06f8e797c47070795630907
David <dgx.arnold@gmail.com>
2024-08-14 11:38:55 +0200

    update(raven): bump sources

--- apps/sources/raven.nix
+++ apps/sources/raven.nix
@@ -1,16 +1,16 @@
 {
   pname = "raven";
-  version = "v1.6.5";
+  version = "v1.6.6";
   meta = {
-    url = "https://github.com/The-Commit-Company/Raven/releases/tag/v1.6.5";
-    description = "Sources for raven (v1.6.5)";
+    url = "https://github.com/The-Commit-Company/Raven/releases/tag/v1.6.6";
+    description = "Sources for raven (v1.6.6)";
   };
   src = builtins.fetchTree {
     type = "github";
     owner = "The-Commit-Company";
     repo = "Raven";
-    narHash = "sha256-TmGHx+PnqzB+opz+GBwMM7u2nwGMzQvD21jVPse275g=";
-    rev = "1a840771215aaade007c8993e23f4ce149322ae3";
+    narHash = "sha256-5kxEEn/zm7a4gqQ6C+wNKDzg0hiHWqzk/K9Vjf5RJCM=";
+    rev = "83ea15d915b5cc6a1561b1ff98353f975bd3c3bd";
   };
   passthru = builtins.fromJSON ''{"since": "v1.0.0", "upstream": "URL: https://github.com/The-Commit-Company/Raven\nPull: +refs/heads/develop:refs/remotes/upstream/develop\nPull: +refs/heads/main:refs/remotes/upstream/main\n"}'';
 }
