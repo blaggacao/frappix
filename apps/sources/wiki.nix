9f6947d15c357865be6adb98c3f30dc3450f21c4
David <dgx.arnold@gmail.com>
2024-08-14 12:28:23 +0200

    build: remove wiki patches

--- apps/sources/wiki.nix
+++ apps/sources/wiki.nix
@@ -1,16 +1,16 @@
 {
   pname = "wiki";
-  version = "20240722.131419";
+  version = "20240814.154017";
   meta = {
-    url = "https://github.com/frappe/wiki/commit/df7b115378575d33677adefc86daf1ee26729005";
-    description = "Sources for wiki (20240722.131419)";
+    url = "https://github.com/frappe/wiki/commit/e1f352102282d8999095a5f554dc73aa96b39284";
+    description = "Sources for wiki (20240814.154017)";
   };
   src = builtins.fetchTree {
     type = "github";
     owner = "frappe";
     repo = "wiki";
-    narHash = "sha256-XnCaOl6RoN078+mhxLcl3rgsd4yiBxY86eXDB3ZuZF0=";
-    rev = "df7b115378575d33677adefc86daf1ee26729005";
+    narHash = "sha256-tNXxlqKOPiqBL/mc7n0O9y3pwVajj+VrNdyusQ34sUY=";
+    rev = "e1f352102282d8999095a5f554dc73aa96b39284";
   };
   passthru = builtins.fromJSON ''{}'';
 }
