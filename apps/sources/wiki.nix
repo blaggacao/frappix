0d7d483cc0cc99c017a93fe05a2a0cbf551abb91
David <dgx.arnold@gmail.com>
2024-08-14 11:23:38 +0200

    update(wiki): bump sources

--- apps/sources/wiki.nix
+++ apps/sources/wiki.nix
@@ -1,16 +1,16 @@
 {
   pname = "wiki";
-  version = "20240121.050421";
+  version = "20240722.131419";
   meta = {
-    url = "https://github.com/frappe/wiki/commit/3899e787aa094b8774738479e60167fb6ba82a7";
-    description = "Sources for wiki (20240121.050421)";
+    url = "https://github.com/frappe/wiki/commit/df7b115378575d33677adefc86daf1ee26729005";
+    description = "Sources for wiki (20240722.131419)";
   };
   src = builtins.fetchTree {
     type = "github";
     owner = "frappe";
     repo = "wiki";
-    narHash = "sha256-mvMrFDD+zbSFRS/ASYJwj0KOj4FN4luZ1P0AsxY+tgY=";
-    rev = "3899e787aa094b8774738479e60167fb6ba82a72";
+    narHash = "sha256-XnCaOl6RoN078+mhxLcl3rgsd4yiBxY86eXDB3ZuZF0=";
+    rev = "df7b115378575d33677adefc86daf1ee26729005";
   };
   passthru = builtins.fromJSON ''{}'';
 }
