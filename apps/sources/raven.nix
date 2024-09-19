1ea32d23f1aaf5b1375ec0714807d897ca832ff6
David <dgx.arnold@gmail.com>
2024-09-19 10:56:33 +0200

    update(raven): to v1.7.0

--- apps/sources/raven.nix
+++ apps/sources/raven.nix
@@ -1,16 +1,16 @@
 {
   pname = "raven";
-  version = "v1.6.6";
+  version = "v1.7.0";
   meta = {
-    url = "https://github.com/The-Commit-Company/Raven/releases/tag/v1.6.6";
-    description = "Sources for raven (v1.6.6)";
+    url = "https://github.com/The-Commit-Company/Raven/releases/tag/v1.7.0";
+    description = "Sources for raven (v1.7.0)";
   };
   src = builtins.fetchTree {
     type = "github";
     owner = "The-Commit-Company";
     repo = "Raven";
-    narHash = "sha256-5kxEEn/zm7a4gqQ6C+wNKDzg0hiHWqzk/K9Vjf5RJCM=";
-    rev = "83ea15d915b5cc6a1561b1ff98353f975bd3c3bd";
+    narHash = "sha256-S8kbFaY2ZsKAF3GK/S/6QbjpA9Piv2LuRbkQ3VVIUKQ=";
+    rev = "08e80357eb16353b349e71ab2971b68cc4f67cab";
   };
   passthru = builtins.fromJSON ''{"since": "v1.0.0", "upstream": "URL: https://github.com/The-Commit-Company/Raven\nPull: +refs/heads/develop:refs/remotes/upstream/develop\nPull: +refs/heads/main:refs/remotes/upstream/main\n"}'';
 }
