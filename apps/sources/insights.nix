81cbc0a45aca6edddb4244985f905eedea5fd460
David <dgx.arnold@gmail.com>
2024-10-25 15:02:47 +0200

    build: expand base image to include all pinned apps

--- apps/sources/insights.nix
+++ apps/sources/insights.nix
@@ -1,16 +1,16 @@
 {
   pname = "insights";
-  version = "v3.0.1-beta";
+  version = "v2.2.6";
   meta = {
-    url = "https://github.com/frappe/insights/releases/tag/v3.0.1-beta";
-    description = "Sources for insights (v3.0.1-beta)";
+    url = "https://github.com/frappe/insights/releases/tag/v2.2.6";
+    description = "Sources for insights (v2.2.6)";
   };
   src = builtins.fetchTree {
     type = "git";
     url = "git@github.com:frappe/insights.git";
     submodules = true;
-    narHash = "sha256-ShwElM2YfMobm4hup/ztgzM+reCpdEMwhxuTJKAOLZ8=";
-    rev = "bf0dd5648f0c20d0760917e9eba1d8662f79392d";
+    narHash = "sha256-ihSI55/2h31GqzhM6E4KvfI3kSJwhpHVOBGar+W8iGY=";
+    rev = "9e8acedb5cdbb75a5f446911519ba777633eac5c";
   };
   passthru = builtins.fromJSON ''{}'';
 }
--- src/oci/testrig.nix
+++ src/oci/testrig.nix
@@ -2,8 +2,16 @@
   oci.frappix = {
     name = "ghcr.io/blaggacao/frappix-test-oci";
     debug = false;
-    apps = with pkgs.frappix; [
-      erpnext
-    ];
+    apps = builtins.attrValues (builtins.removeAttrs pkgs.frappix [
+      "appSources"
+      "callPackage"
+      "newScope"
+      "overrideScope"
+      "overrideScope'"
+      "builder" # brakes via https://github.com/frappe/builder/commit/cae39ff422812b52d3c1b25ae4756669add794d1#commitcomment-148362353
+      "crm" # brakes via https://github.com/frappe/crm/commit/a439433977321188266ff38cdef09642e4166080#commitcomment-148363342
+      "drive" # waiting on https://github.com/blaggacao/frappix/pull/11
+      "hrms" # brakes via https://github.com/frappe/hrms/issues/2342
+    ]);
   };
 }
