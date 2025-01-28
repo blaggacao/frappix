b342c69190b5f23b08e8d271d4be633c993b1ba8
David <dgx.arnold@gmail.com>
2025-01-28 19:12:07 +0100

    fix: oci testrig enabled apps

--- src/oci/testrig.nix
+++ src/oci/testrig.nix
@@ -1,17 +1,17 @@
-{pkgs, ...}: {
+inputs: {pkgs, ...}: {
   oci.frappix = {
     name = "ghcr.io/blaggacao/frappix-test-oci";
     debug = false;
-    apps = builtins.attrValues (builtins.removeAttrs pkgs.frappix [
-      "appSources"
-      "callPackage"
-      "newScope"
-      "overrideScope"
-      "overrideScope'"
-      "builder" # brakes via https://github.com/frappe/builder/commit/cae39ff422812b52d3c1b25ae4756669add794d1#commitcomment-148362353
-      "crm" # brakes via https://github.com/frappe/crm/commit/a439433977321188266ff38cdef09642e4166080#commitcomment-148363342
-      "drive" # waiting on https://github.com/blaggacao/frappix/pull/11
-      "hrms" # brakes via https://github.com/frappe/hrms/issues/2342
-    ]);
+    apps = let
+      appList = builtins.attrNames (builtins.removeAttrs inputs.cells.apps.sources [
+        "bench" # not an app
+        "frappe" # automatically added; don't add twice
+        "builder" # brakes via https://github.com/frappe/builder/commit/cae39ff422812b52d3c1b25ae4756669add794d1#commitcomment-148362353
+        "crm" # brakes via https://github.com/frappe/crm/commit/a439433977321188266ff38cdef09642e4166080#commitcomment-148363342
+        "drive" # waiting on https://github.com/blaggacao/frappix/pull/11
+        "hrms" # brakes via https://github.com/frappe/hrms/issues/2342
+      ]);
+    in
+      map (name: pkgs.frappix.${name}) appList;
   };
 }
