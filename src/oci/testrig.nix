39c5c36346e607d048830afaf9756307b56d649b
David <dgx.arnold@gmail.com>
2025-01-28 21:29:47 +0100

    build(hrms): bump to fixed version

--- src/oci/testrig.nix
+++ src/oci/testrig.nix
@@ -9,7 +9,6 @@ inputs: {pkgs, ...}: {
         "builder" # brakes via https://github.com/frappe/builder/commit/cae39ff422812b52d3c1b25ae4756669add794d1#commitcomment-148362353
         "crm" # brakes via https://github.com/frappe/crm/commit/a439433977321188266ff38cdef09642e4166080#commitcomment-148363342
         "drive" # waiting on https://github.com/blaggacao/frappix/pull/11
-        "hrms" # brakes via https://github.com/frappe/hrms/issues/2342
         "gameplan" # brakes via https://github.com/blaggacao/frappix/issues/18#issuecomment-2619829870
         "insights" # brakes via https://github.com/blaggacao/frappix/issues/18#issuecomment-2619860284
       ]);
