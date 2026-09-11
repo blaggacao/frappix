dbfa76d84df6354bd7528c9d0c407387f66c3e33
kurogeek <kurogeek@lmvhaus.com>
2026-09-11 06:03:09 +0000

    update: frappe -> v15.120.1, erpnext -> v15.121.2

--- apps/sources/erpnext.nix
+++ apps/sources/erpnext.nix
@@ -1,5 +1,5 @@
 let
-version = "v15.119.3";
+version = "v15.121.2";
 in
 {
   pname = "erpnext";
@@ -11,8 +11,8 @@ in
   src = builtins.fetchTree {
     type = "github";
     owner = "frappe"; repo = "erpnext";
-    narHash = "sha256-kkO9h1yZ2+KM+Q6NbNiYqeTNfbae0cOEjARIykH1EFQ=";
-    rev = "0526834071315bbbd6b1ae52511c0b4659f17395";
+    narHash = "sha256-uaoDRljCKtyaTikd2TrFv/9dlvZLZYcNDPNLAgLXCq8=";
+    rev = "df8b7f9648c2ec4da12db8c4022edc8dd1018c6b";
   };
   passthru = builtins.fromJSON ''{"since": "version-14", "upstream": "URL: https://github.com/frappe/erpnext\nPull: +refs/heads/develop:refs/remotes/upstream/develop\nPull: +refs/heads/version-15:refs/remotes/upstream/version-15\nPull: +refs/heads/version-15-hotfix:refs/remotes/upstream/version-15-hotfix\nPull: +refs/tags/v15.*:refs/remotes/upstream/tags/v15.*\n"}'';
 }
--- apps/sources/frappe.nix
+++ apps/sources/frappe.nix
@@ -1,5 +1,5 @@
 let
-version = "v15.118";
+version = "v15.120.1";
 in
 {
   pname = "frappe";
@@ -11,8 +11,8 @@ in
   src = builtins.fetchTree {
     type = "github";
     owner = "frappe"; repo = "frappe";
-    narHash = "sha256-VcZM09M20wO0EXRNOHcv1O1A1A1niMXSOyw/xtdA6lI=";
-    rev = "9b8d265b27a1dfb11c7aef21a533a127e14a0a5a";
+    narHash = "sha256-dI4BQN2m5G1lPR1Ausmfpkut6SFPP+MONoEBPI5cMFU=";
+    rev = "9f8ae9cd25b6735be345da6cc12e9f5a96050c68";
   };
   passthru = builtins.fromJSON ''{"clone": {"since": "version-14", "upstream": {"fetch": ["+refs/heads/develop:refs/remotes/upstream/develop", "+refs/heads/version-15:refs/remotes/upstream/version-15", "+refs/heads/version-15-hotfix:refs/remotes/upstream/version-15-hotfix", "+refs/tags/v15.*:refs/remotes/upstream/tags/v15.*"], "url": "https://github.com/frappe/frappe"}}}'';
 }
